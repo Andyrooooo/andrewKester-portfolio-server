6ea04c24c3ee7cf7b343dddb4acffbc8f486a8e4
Anyrooooo <10919987@my.uvu.edu>
2024-02-19 16:32:26 -0700

    blah blah blah

--- api/send.js
+++ api/send.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(dotenv.config());
+
 
 app.post('/send', (req, res) => {
     const formData = req.body
